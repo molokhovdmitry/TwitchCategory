cd2f78229040c5b650e3e71f91eee12045f4635c
Dmitry Molokhov <molokhovdmitry@gmail.com>
2021-03-04 20:01:27 +0300

    Finish minDataCategory()

--- data/dbFuncs.py
+++ data/dbFuncs.py
@@ -30,7 +30,7 @@ def updateGames(session, games):
     """
     Updates `games` table.
 
-    `games` is a dictionary of format:
+    `games` input is a dictionary of format:
     {game_id: game_name}
     """
     for game in games:
@@ -45,14 +45,29 @@ def updateGames(session, games):
             game = Game(id=id, name=name)
             session.add(game)
     
-    """
     # Print all games
     for game in session.query(Game).all():
         print(game)
+
+def minDataCategory(session):
+    """Finds a category (game ID) with minimum amount of frames."""
+
+    return session.query(GameFrames.game_id).\
+           order_by(GameFrames.frame_count).first()[0]
+
+    """
+    game1 = GameFrames(game_id="1654", frame_count="1234")
+    game3 = GameFrames(game_id="29975", frame_count="0")
+    game2 = GameFrames(game_id="19554", frame_count="23453245")
+
+    session.add_all([game1, game3, game2])
+
+    # Print
+    for game in session.query(GameFrames).all():
+        print(game)
     """
 
-def minDataCategory():
-    raise NotImplementedError
+
 
 def addFrame():
     raise NotImplementedError
