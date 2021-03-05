fc35dde0e077b1d1ef27cbd82deee6d890ee8048
Dmitry Molokhov <molokhovdmitry@gmail.com>
2021-03-05 16:10:28 +0300

    Finish downloadStream()

--- data/download.py
+++ data/download.py
@@ -1,12 +1,56 @@
+"""
+This file downloads stream segments and saves frames.
+
+Functions:
+1) Download stream segments, ensure they're not an ad.
+2) Get frames from a segment and save them in filesystem and database.
+"""
+
 import streamlink
+import requests
 from subprocess import Popen
 from time import sleep
 import sys
+import re
 
-stream_url = streamlink.streams("https://www.twitch.tv/vadikus007")["best"].url
 
-ffmpeg_process = Popen(["ffmpeg", "-i", stream_url, "-c", "copy", "stream.mkv"])
+def downloadStream(login):
+    """
+    Download stream segments in 480p quality.
+    
+    Returns `None` if gets an ad.
+    """
 
-sleep(5)
+    """
+    Get a dictionary of format {`quality`: `url`} containing `.m3u8` file urls for every quality.
+    """
 
-ffmpeg_process.kill()
+    links = streamlink.streams(f"https://www.twitch.tv/{login}")
+
+    # Get stream qualities
+    qualities = [quality for quality in links]
+    if "480p" in qualities:
+        # Get 480p `.m3u8` url
+        m3u8 = links["480p"].url
+    
+    # Get `.m3u8` file
+    response = requests.get(m3u8).text
+    # Ensure that `.m3u8` file links to stream source and not to ads
+    if response.lower().count("twitch-ad") > 1:
+        print("Ad")
+        return None
+    else:
+        # Get segment links
+        links = re.findall(r'(https?://\S+)', response)[1:-2]
+
+        # Download and save all segments
+        for link, i in zip(links, range(1, len(links) + 1)):
+            # Request `.ts` file
+            segment = requests.get(link).content
+            name = f"segment{i}.ts"
+            with open(name, 'wb') as f:
+                f.write(segment)
+
+
+def saveFrames(segment):
+    raise NotImplementedError
--- data/main.py
+++ data/main.py
@@ -1,3 +1,4 @@
+from download import *
 from api import *
 from dbFuncs import *
 
@@ -6,3 +7,10 @@ with sessionScope() as session:
     #print(minDataCategory(session))
     #addFrame(session, "/aasdfsdf/gsasdfdf/453345.png", "500188", "pokelawls")
     print
+
+"""
+for streamer in getStreamers(9618):
+    print(streamer)
+    downloadStream(streamer)
+"""
+downloadStream("nymn")
