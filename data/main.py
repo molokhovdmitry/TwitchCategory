d3eeb60e89c5a600d120339177e3babb7a7df7ff
Dmitry Molokhov <molokhovdmitry@gmail.com>
2021-03-11 19:03:20 +0300

    Polish main.py

--- data/main.py
+++ data/main.py
@@ -1,16 +1,25 @@
+"""
+This file gets top viewed games of twitch, adds them to database.
+Then it finds the game with the least amount of saved frames and
+finds streams in that category. After that it tries to download
+frames from 5 random streams.
+"""
+
+
 from download import downloadFrames
 from api import *
 from dbFuncs import *
 
+"""
+Make a thread that will stop the while loop on input.
+"""
 import threading
-
-import random
 
 def inputThread(inputList):
     """Thread that waits for an input."""
     input()
-    print("Interrupting.")
     inputList.append(True)
+    print("Interrupting.")
 
 """Start a thread that updates `inputList` with inputs."""
 inputList = []
@@ -18,41 +27,45 @@ threading.Thread(target=inputThread, args=(inputList, )).start()
 print("Press Enter to stop downloading.")
 
 
-# Update data while no input (Enter not pressed)
+"""Update data while no input (Enter not pressed)."""
 while not inputList:
     
     with sessionScope() as session:
-        # Update top categories
+
+        """Update top categories."""
         games = getTopGames()
         updateGames(session, games)
         updateFrameCount(session)
 
-        # Get category with least data
+        """Get category with least data."""
         gameID = minDataCategory(session)
 
-        # Update category (download frames 5 times)
+        """Update category (download frames 5 times)."""
+        
         downloadCount = 0
         downloadAttempts = 0
-        # Get streams from category
+
+        """Get streams from category."""
         streams = getStreams(gameID)
         while streams and downloadCount < 5 and downloadAttempts < 10:
 
-            # Get random stream
+            """Get random stream."""
             stream = streams.pop()
 
             print(f"Stream: {stream}, gameID: {gameID}")
 
-            # Download frames from stream, update database
+            """Download frames from stream, update database."""
             download = False
             for framePath in downloadFrames(stream, gameID):
                 download = True
-                # Save frame in database
+                """Save frame in database."""
                 addFrame(session, framePath, gameID, stream)
             
             downloadCount += download
             downloadAttempts += 1
-        
+
+"""Update database."""
 with sessionScope() as session:
     updateFrameCount(session)
 
-print("Completed.")
+print("Done.")
