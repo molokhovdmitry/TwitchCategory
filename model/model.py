ffb8cc7ac3b8a76cfade0d5c194bfc2dd11caf99
Dmitry Molokhov <molokhovdmitry@gmail.com>
2021-03-17 21:12:12 +0300

    Finish model

--- model/model.py
+++ model/model.py
@@ -1,13 +1,11 @@
-from config import DATA_PATH
-import matplotlib.pyplot as plt
-import numpy as np
-import os
-import PIL
-import tensorflow as tf
+"""
+This file creates the model.
+"""
 
-from tensorflow import keras
+import tensorflow as tf
 from tensorflow.keras import layers
 from tensorflow.keras.models import Sequential
+import matplotlib.pyplot as plt
 
 """
 GPU support fix.
@@ -17,105 +15,118 @@ config = tf.compat.v1.ConfigProto()
 config.gpu_options.allow_growth = True
 session = tf.compat.v1.Session(config=config)
 
-
-import sys
-
-
 from config import DATA_PATH
-import pathlib
-directory = pathlib.Path(DATA_PATH)
-
-#imageCount = len(list(directory.glob("*/*.jpg")))
-
-#game = list(directory.glob('941530474/*'))
-#image = PIL.Image.open(str(game[105]))
-#image.show()
-
+EPOCHS = 10
 IMG_HEIGHT = 180
 IMG_WIDTH = 180
 
-train_ds = tf.keras.preprocessing.image_dataset_from_directory(
-    DATA_PATH,
-    validation_split=0.2,
-    subset="training",
-    seed=123,
-    image_size=(IMG_HEIGHT, IMG_WIDTH),
-    batch_size=64
-)
-
-val_ds = tf.keras.preprocessing.image_dataset_from_directory(
-    DATA_PATH,
-    validation_split=0.2,
-    subset="validation",
-    seed=123,
-    image_size=(IMG_HEIGHT, IMG_WIDTH),
-    batch_size=32
-)
-
-class_names = train_ds.class_names
-print(class_names)
-
-sys.exit()
-num_classes = len(class_names)
-
-AUTOTUNE = tf.data.AUTOTUNE
-
-train_ds = train_ds.cache().shuffle(10).prefetch(buffer_size=AUTOTUNE)
-val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
+"""Get all classes."""
+import pathlib
+CLASS_NAMES = [category.name for category in pathlib.Path(DATA_PATH).iterdir()]
+NUM_CLASSES = len(CLASS_NAMES)
 
 
-model = Sequential([
-    layers.experimental.preprocessing.Rescaling(1./255, input_shape=(IMG_HEIGHT, IMG_WIDTH, 3)),
-    layers.Conv2D(64, 3, padding='same', activation='relu'),
-    layers.MaxPooling2D(),
-    layers.Conv2D(64, 3, padding='same', activation='relu'),
-    layers.MaxPooling2D(),
-    layers.Conv2D(64, 3, padding='same', activation='relu'),
-    layers.MaxPooling2D(),
-    layers.Conv2D(64, 3, padding='same', activation='relu'),
-    layers.MaxPooling2D(),
-    layers.Flatten(),
-    layers.Dense(256, activation='relu'),
-    layers.Dropout(0.5),
-    layers.Dense(num_classes)
-])
+def main():
+    """Create a model."""
 
-model.compile(
-    optimizer='adam',
-    loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-    metrics=['accuracy']
-)
+    train_ds, val_ds = load_data(DATA_PATH)
 
-model.summary()
+    model = get_model()
 
-epochs = 10
-history = model.fit(
-    train_ds,
-    validation_data=val_ds,
-    epochs=epochs
-)
+    model.summary()
 
-"""Save model to file."""
-model.save("model.h5")
-print("Model saved.")
+    """Fit the model and save the history."""
+    history = model.fit(train_ds, validation_data=val_ds, epochs=EPOCHS)
 
-acc = history.history['accuracy']
-val_acc = history.history['val_accuracy']
+    """Save the model to a file."""
+    model.save("model.h5")
+    print("Model saved.")
 
-loss = history.history['loss']
-val_loss = history = history.history['val_loss']
-epochs_range = range(epochs)
+    """Make loss and accuracy plots on the training and validation sets."""
+    make_plots(history, EPOCHS)
 
-plt.figure(figsize=(8, 8))
-plt.subplot(1, 2, 1)
-plt.plot(epochs_range, acc, label="Training Accuracy")
-plt.plot(epochs_range, val_acc, label="Validation Accuracy")
-plt.legend(loc="lower right")
-plt.title("Training and Validation Accuracy")
 
-plt.subplot(1, 2, 2)
-plt.plot(epochs_range, loss, label="Traing Loss")
-plt.plot(epochs_range, val_loss, label="Validation Loss")
-plt.legend(loc="upper right")
-plt.title("Training and Validation Loss")
-plt.show()
+def load_data(data_dir):
+    """Load the data. Return tuple (`train_ds`, `val_ds`)."""
+
+    train_ds = tf.keras.preprocessing.image_dataset_from_directory(
+        data_dir,
+        validation_split=0.2,
+        subset="training",
+        seed=123,
+        image_size=(IMG_HEIGHT, IMG_WIDTH),
+        batch_size=64
+    )
+
+    val_ds = tf.keras.preprocessing.image_dataset_from_directory(
+        data_dir,
+        validation_split=0.2,
+        subset="validation",
+        seed=123,
+        image_size=(IMG_HEIGHT, IMG_WIDTH),
+        batch_size=32
+    )
+
+    AUTOTUNE = tf.data.AUTOTUNE
+    train_ds = train_ds.cache().shuffle(50).prefetch(buffer_size=AUTOTUNE)
+    val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
+
+    return train_ds, val_ds
+
+
+def get_model():
+    """Create and compile neural network."""
+
+    model = Sequential([
+        layers.experimental.preprocessing.Rescaling(1./255, input_shape=(IMG_HEIGHT, IMG_WIDTH, 3)),
+        layers.Conv2D(64, 3, padding='same', activation='relu'),
+        layers.MaxPooling2D(),
+        layers.Conv2D(64, 3, padding='same', activation='relu'),
+        layers.MaxPooling2D(),
+        layers.Conv2D(64, 3, padding='same', activation='relu'),
+        layers.MaxPooling2D(),
+        layers.Conv2D(64, 3, padding='same', activation='relu'),
+        layers.MaxPooling2D(),
+        layers.Flatten(),
+        layers.Dense(256, activation='relu'),
+        layers.Dropout(0.5),
+        layers.Dense(NUM_CLASSES),
+    ])
+
+    model.compile(
+        optimizer='adam',
+        loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+        metrics=['accuracy']
+    )
+
+    return model
+
+
+def make_plots(history, epochs):
+    """Visualize training results."""
+
+    acc = history.history['accuracy']
+    val_acc = history.history['val_accuracy']
+
+    loss = history.history['loss']
+    val_loss = history = history.history['val_loss']
+    epochs_range = range(epochs)
+
+    plt.figure(figsize=(8, 8))
+
+    plt.subplot(1, 2, 1)
+    plt.plot(epochs_range, acc, label="Training Accuracy")
+    plt.plot(epochs_range, val_acc, label="Validation Accuracy")
+    plt.legend(loc="lower right")
+    plt.title("Training and Validation Accuracy")
+
+    plt.subplot(1, 2, 2)
+    plt.plot(epochs_range, loss, label="Traing Loss")
+    plt.plot(epochs_range, val_loss, label="Validation Loss")
+    plt.legend(loc="upper right")
+    plt.title("Training and Validation Loss")
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
