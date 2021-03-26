c86910d6a3d57c674fbdb9fcc80105a8efc0711e
Dmitry Molokhov <molokhovdmitry@gmail.com>
2021-03-26 16:23:38 +0300

    Polish dbFuncs.py

--- data/dbFuncs.py
+++ data/dbFuncs.py
@@ -3,10 +3,12 @@ This file has a context manager for managing database sessions and
 functions to contact the database.
 
 Functions:
-    1) Update `games` table with new games.
-    2) Update `frames` for every game in `games` table.
-    2) Find a category (game ID) with minimum amount of frames.
-    3) Add frame information to `frames` table.
+    1) Session context manager.
+    2) Update `games` table with new games.
+    3) Update `frames` (frame count) for every game in `games` table.
+    4) Find a category (game ID) with minimum amount of frames.
+    5) Add frame information to `frames` table.
+    6) Get game name from game ID. (not used)
 """
 
 from contextlib import contextmanager
@@ -18,7 +20,7 @@ from data.db import Game, Frame
 @contextmanager
 def sessionScope():
     """
-    Provide a transactional scope around a series of operation.
+    Provide a transactional scope around a series of operations.
     
     This context manager constructs a session, commits transactions and
     closes the session.    
@@ -38,9 +40,7 @@ def sessionScope():
 
 def updateGames(session, games):
     """
-    Update `games` table.
-
-    `games` input is a dictionary of format:
+    Update `games` table with `games` dictionary of format:
     {game_id: game_name}
     """
 
@@ -52,39 +52,39 @@ def updateGames(session, games):
         """Check if the game already exists in the database."""
         if not session.query(Game).filter_by(id=id).all():
 
-            """Insert game in tables if not exists."""
+            """Insert the game in the table."""
             game = Game(id=id, name=name, frames=0)
             session.add(game)
 
 
 def updateFrameCount(session):
-    """Update `frames` for every game in `games` table."""
+    """Update `frames` (frame count) for every game in `games` table."""
 
     """Get all games."""
     for game in session.query(Game).all():
 
-        """Get game frames."""
+        """Get game frame amount."""
         frames = session.query(Frame).filter_by(game_id=game.id).count()
 
-        """Update frames."""
+        """Update game `frames`."""
         session.query(Game).filter_by(id=game.id).\
             update({Game.frames: frames}, synchronize_session=False)
 
 
 def minDataCategory(session):
-    """Find a category (game ID) with minimum amount of frames."""
+    """Find a category (game ID) with a minimum amount of frames."""
 
     return session.query(Game.id).order_by(Game.frames).first()[0]
 
 
-def addFrame(session, path, game_id, user_login):
+def addFrame(session, path, gameID, login):
     """Add frame information to `frames` table."""
 
-    frame = Frame(path=path, game_id=game_id, user_login=user_login)
+    frame = Frame(path=path, game_id=gameID, user_login=login)
     session.add(frame)
 
 
 def gameIDtoName(session, gameID):
-    """Converts game ID to name using the database."""
+    """Converts game ID to name."""
 
     return session.query(Game.name).filter_by(id=gameID).first()[0]
