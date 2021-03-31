895b968e37e5fc906d4f82a28ddc20b261cc77fe
Dmitry Molokhov <molokhovdmitry@gmail.com>
2021-03-31 17:05:52 +0300

    Polish download, os to pathlib

--- data/download.py
+++ data/download.py
@@ -4,39 +4,36 @@ has function that downloads frames from stream,
 resizes them to required resolution and saves them.
 
 Functions:
-1) Download stream segments, ensure they're not an ad.
-2) Get frames from a segment and save them in filesystem and database.
+1) Download stream segments, extract and save frames.
+2) Helper function. Returns the last added frame number for a directory.
+3) Deletes files in `temp` directory.
 """
 
 import os
-import pathlib
 import requests
 import re
+from pathlib import Path
+import sys ###########
 
-import streamlink
-from streamlink import Streamlink
 import cv2 as cv
 
-
-"""Saved image resolution."""
-IMG_HEIGHT = 480
-IMG_WIDTH = 854
-
-
-"""Create required folders if not exist."""
+from config import IMG_SIZE
 from config import DOWNLOAD_PATH
 
-downloadPath = DOWNLOAD_PATH
-framesPath = downloadPath + f"frames{os.sep}"
-tempPath = downloadPath + f"temp{os.sep}"
+IMG_HEIGHT = IMG_SIZE["height"]
+IMG_WIDTH = IMG_SIZE["width"]
+
+downloadPath = Path(DOWNLOAD_PATH)
+framesPath = Path.joinpath(downloadPath, "frames")
+tempPath = Path.joinpath(downloadPath, "temp")
 
 """Ensure paths exist."""
-if not os.path.exists(downloadPath):
-    os.mkdir(downloadPath)
-if not os.path.exists(framesPath):
-    os.mkdir(framesPath)
-if not os.path.exists(tempPath):
-    os.mkdir(tempPath)
+if not Path.exists(downloadPath):
+    Path.mkdir(downloadPath)
+if not Path.exists(framesPath):
+    Path.mkdir(framesPath)
+if not Path.exists(tempPath):
+    Path.mkdir(tempPath)
 
 
 def downloadFrames(streamlinkSession, login, gameID=None):
@@ -48,14 +45,14 @@ def downloadFrames(streamlinkSession, login, gameID=None):
     else saves in `framesPath`/`temp`.
     
     Yields saved frame paths.
-    Returns `None` if gets an ad.
+    Returns `None` if can't get frames.
     """
 
     if gameID:
         """Create `gameID` folder if not exists."""
-        downloadPath = f"{framesPath}{gameID}{os.sep}"
-        if not os.path.exists(downloadPath):
-            os.mkdir(downloadPath)
+        downloadPath = Path.joinpath(framesPath, str(gameID))
+        if not Path.exists(downloadPath):
+            Path.mkdir(downloadPath)
     else:
         downloadPath = tempPath
     
@@ -64,89 +61,95 @@ def downloadFrames(streamlinkSession, login, gameID=None):
     `.m3u8` file urls for every quality.
     """
     try:
-        links = streamlinkSession.streams(f"https://www.twitch.tv/{login}")
+        m3u8Links = streamlinkSession.streams(f"https://www.twitch.tv/{login}")
     except:
-        print("Streamlink couldn't get a stream.")
+        print(login)
+        print(f"Streamlink couldn't get {login}'s stream.")
         return None
 
-    """Get the best quality `.m3u8` url."""
-    if not links:
-        print("Streamer not live.")
+    """Ensure the stream is live."""
+    if not m3u8Links:
+        print("Stream not live.")
         return None
-    m3u8 = links["best"].url
     
-    """Get `.m3u8` file."""
+    """Get the best quality `.m3u8` url."""
+    m3u8 = m3u8Links["best"].url
+    
+    """Request `.m3u8` file."""
     response = requests.get(m3u8).text
+    
     """Ensure that `.m3u8` file links to stream source and not to ads."""
     if response.lower().count("twitch-ad") > 1:
         print("Ad.")
         return None
 
     """Get segment links."""
-    links = re.findall(r'(https?://\S+)', response)[2:-2]
+    segLinks = re.findall(r'(https?://\S+)', response)[2:-2]
 
     """Download and save all frames from segments."""
     frameNumber = lastAddedNum(downloadPath) + 1
-    for link, i in zip(links, range(1, len(links) + 1)):
+    for link, i in zip(segLinks, range(1, len(segLinks) + 1)):
 
         """Request `.ts` file."""
         segment = requests.get(link).content
 
-        """Save file."""
-        segmentPath = f"{tempPath}segment{i}.ts"
-        with open(segmentPath, 'wb') as f:
-            f.write(segment)
+        """Save the file."""
+        segmentPath = Path.joinpath(tempPath, f"segment{i}.ts")
+        with open(segmentPath, 'wb') as file:
+            file.write(segment)
 
         try:
             """Open segment with cv."""
-            video = cv.VideoCapture(segmentPath)
+            video = cv.VideoCapture(str(segmentPath))
 
-            """Get first frame and release video."""
+            """Get the first frame and release the video."""
             frame = video.read()[1]
             video.release()
 
-            """Resize frame and save."""
-            frame = cv.resize(frame, (IMG_WIDTH, IMG_HEIGHT), interpolation=cv.INTER_AREA)
-            framePath = f"{downloadPath}{frameNumber}.jpg"
-            cv.imwrite(framePath, frame)
+            """Resize and save the frame."""
+            frame = cv.resize(frame,
+                              (IMG_WIDTH, IMG_HEIGHT),
+                              interpolation=cv.INTER_AREA)
+            framePath = Path.joinpath(downloadPath, f"{frameNumber}.jpg")
+            cv.imwrite(str(framePath), frame)
             frameNumber += 1
             
             if gameID:
                 """Yield path for saving in the database."""
-                yield f"{gameID}{os.sep}{os.path.basename(framePath)}"
+                yield  str(Path.joinpath(Path(str(gameID)), framePath.name))
             else:
                 """Yield path for recognition."""
-                yield framePath
+                yield str(framePath)
 
         except:
             print("Error. Couldn't get a frame from segment.")
 
         finally:
             """Delete segment."""
-            os.remove(segmentPath)
+            segmentPath.unlink()
     
     print("Downloaded frames.")
 
 
-def lastAddedNum(downloadPath):
+def lastAddedNum(path):
     """
-    Return last added frame number for a `downloadPath` directory.
+    Return last added frame number for a `path` directory.
+    `path` is a pathlib.Path object.
 
     Return 0 if no files in directory.
     """
 
-    files = os.listdir(downloadPath)
+    files = list(path.iterdir())
     if not files:
         return 0
     
     """Get all frame numbers and return max."""
-    numbers = [int(file.rstrip('.jpg')) for file in files]
+    numbers = [int(str(file.name).rstrip('.jpg')) for file in files]
     return max(numbers)
 
 
 def delTempFiles():
     """Delete files in `tempPath`."""
     
-    path = pathlib.Path(tempPath)
-    for file in list(path.glob("*")):
+    for file in list(tempPath.iterdir()):
         file.unlink()
