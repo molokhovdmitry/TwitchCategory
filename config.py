c1e40a36b644bc533caa445e177f2bc5ad75e878
Dmitry Molokhov <molokhovdmitry@gmail.com>
2021-04-08 16:55:27 +0300

    Add config template

---
+++ config.py
@@ -0,0 +1,24 @@
+"""API"""
+CLIENT_ID = ""
+ACCESS_TOKEN = ""
+
+"""Database"""
+DB_NAME = ""
+DB_HOST = "localhost"
+DB_PORT = "5432"
+DB_USER = ""
+DB_PASSWORD = ""
+
+"""Data download path"""
+DOWNLOAD_PATH = ""
+
+"""Image size"""
+IMG_SIZE = {"height": 240, "width": 240}
+
+"""Maximum amount of categories"""
+MAX_GAMES = 50
+
+"""Model settings"""
+MODEL_PATH = "model/"
+EPOCHS = 10
+
