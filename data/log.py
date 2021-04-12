61102d7c808a186917eb16937cceab5dac16136b
Dmitry Molokhov <molokhovdmitry@gmail.com>
2021-04-12 16:14:51 +0300

    Add log.py

---
+++ data/log.py
@@ -0,0 +1,44 @@
+"""
+MIT License
+
+Copyright (c) 2021 molokhovdmitry
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+"""This file creates a log file and writes in it."""
+
+from pathlib import Path
+
+path = Path.joinpath(Path("data"), "log.txt")
+
+
+def log(*args):
+    """Writes `text` to a log file."""
+
+    """Create file if not exists."""
+    if not path.exists():
+        print("Created data/log.txt file.")
+        path.touch()
+
+    """Write in file."""
+    with path.open("a") as f:
+        for arg in args:
+            f.write(f"{arg}\n")
+    f.write("\n")
